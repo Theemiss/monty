72c7be4f84b5e91f34c375b512d1c1b01c8a3821
Your Name <midinfotn401@gmail.com>
2020-12-21 13:58:24 +0100

    pstr

--- pstr.c
+++ pstr.c
@@ -13,18 +13,23 @@ void pstr_monty(stack_t **stack, unsigned int line_number)
 
 	node = *stack;
 	if (*stack == NULL)
-		putchar('\n');
-	for (i = 0; node; i++)
 	{
-		if (node->n >= 65 && node->n <= 90)
-			putchar(node->n);
-		else if (node->n >= 97 && node->n <= 122)
-			putchar(node->n);
-		else if (node->n == 0)
-			node = node->next;
-		else
-			break;
-		node = node->next;
+		putchar('\n');
 	}
-	putchar('\n');
+	else
+	{
+		for (i = 0; node; i++)
+		{
+			if (node->n >= 65 && node->n <= 90)
+				putchar(node->n);
+			else if (node->n >= 97 && node->n <= 122)
+				putchar(node->n);
+			else if (node->n == 0)
+				node = node->next;
+			else
+				break;
+		node = node->next;
+		}
+		putchar('\n');
+	}
 }
