ee343f18cf50f0879cd017fe12b79fc14a134fc7
Your Name <midinfotn401@gmail.com>
2020-12-21 15:23:32 +0100

    rotr

--- rotl_and_rotr.c
+++ rotl_and_rotr.c
@@ -19,3 +19,25 @@ void rotl_monty(stack_t **stack, unsigned int line_number)
 	}
 	head->n = temp;
 }
+/**
+ * rotr_monty - rotates the stack to the bottom.
+ * @stack: Pointer To The head
+ * @line_number: The Line Number
+ * Return:Void
+ */
+void rotr_monty(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+	int temp = 0;
+	(void)line_number;
+
+	while (head->next != NULL)
+		head = head->next;
+	temp = head->n;
+	while (head->prev != NULL)
+	{
+		head = head->prev;
+		head->next->n = head->n;
+	}
+	head->n = temp;
+}
